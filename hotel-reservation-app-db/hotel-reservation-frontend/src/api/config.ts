4ad1305a2ea57e16576a8a339dc9b5b410ff3e57
maryl-collab <maryl@wso2.com>
2025-10-21 17:34:44 -0300

    Update config.ts
    adicionando url api

--- hotel-reservation-app-db/hotel-reservation-frontend/src/api/config.ts
+++ hotel-reservation-app-db/hotel-reservation-frontend/src/api/config.ts
@@ -6,4 +6,4 @@ declare global {
   }
 }
 
-export const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "http://hotel-reservation-service-3730685075:4000/api/reservations";
+export const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "https://0def71b9-85bb-4456-be27-e5a8b41a3445-dev.e1-us-east-azure.choreoapis.dev/luxury-hotels/hotel-reservation-service/v1.0";
